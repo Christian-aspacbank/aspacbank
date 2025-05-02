ccd74cdb34bacd951a3de00af35c4a5abbe1db22
Christian-aspacbank <ryanchristianbitoon1996@gmail.com>
2025-05-02 11:29:02 +0800

    Update code

--- src/Pages/Branches.tsx
+++ src/Pages/Branches.tsx
@@ -7,7 +7,7 @@ const branches = [
     address:
       "ASPAC RURAL BANK Bldg. Cor. M.C. Briones Highway & Gen. Ricarte Sts. Guizo 6014 Mandaue City Cebu Philippines",
     contact: "(032) 345-0929/345-0930",
-    hours: "9:00 AM - 5:00 PM (Mon-Fri)",
+    hours: "9:00 AM - 3:00 PM (Mon-Fri)",
     mapUrl:
       "https://www.google.com/maps/place/10%C2%B020'05.6%22N+123%C2%B056'23.4%22E/@10.334875,123.939833,18z/data=!4m4!3m3!8m2!3d10.3348754!4d123.9398333?hl=en-US&entry=ttu&g_ep=EgoyMDI1MDMyNS4xIKXMDSoASAFQAw%3D%3D",
     image: "/Mandaue.jpg",
@@ -17,7 +17,7 @@ const branches = [
     address:
       "ASPAC BANK Bldg. Cor. N. Bacalso St. & P. Del Rosario Ext., Sambag 1 (Pob.) 6000 Cebu City (Capital) Cebu Philippines",
     contact: "(0917-102-5671)",
-    hours: "9:00 AM - 5:00 PM (Mon-Fri)",
+    hours: "9:00 AM - 3:00 PM (Mon-Fri)",
     mapUrl:
       "https://www.google.com/maps/place/10%C2%B020'18.7%22N+123%C2%B054'42.0%22E/@10.3385492,123.911637,3a,75y,140.89h,90t/data=!3m7!1e1!3m5!1sXmNZr6TyAw_ZeLc-zYzrCA!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D0%26panoid%3DXmNZr6TyAw_ZeLc-zYzrCA%26yaw%3D140.89452!7i16384!8i8192!4m4!3m3!8m2!3d10.3385262!4d123.9116589?hl=en-US&entry=ttu&g_ep=EgoyMDI1MDQyNy4xIKXMDSoASAFQAw%3D%3D",
     image: "/Banilad.jpg",
@@ -27,7 +27,7 @@ const branches = [
     name: "Bantayan Branch",
     address: "Ticad (Pob.) 6052 Bantayan Cebu Philippines",
     contact: "(0917-128-4422)",
-    hours: "9:00 AM - 5:00 PM (Mon-Fri)",
+    hours: "9:00 AM - 3:00 PM (Mon-Fri)",
     mapUrl:
       "https://www.google.com/maps/place/11%C2%B010'10.0%22N+123%C2%B043'26.7%22E/@11.169456,123.72407,18z",
     image: "/Bantayan.jpg",
@@ -39,7 +39,7 @@ const branches = [
     address:
       "ASPAC BANK Bldg. P. Rodriguez St. Cogon (Pob.) 6010 City of Bogo Cebu Philippines",
     contact: "(0917-129-4966)",
-    hours: "9:00 AM - 5:00 PM (Mon-Fri)",
+    hours: "9:00 AM - 3:00 PM (Mon-Fri)",
     mapUrl:
       "https://www.google.com/maps?ll=11.046723,124.002811&z=18&t=m&hl=en-US&gl=US&mapclient=embed&q=11%C2%B002%2748.2%22N+124%C2%B000%2710.1%22E+11.046723,+124.002811@11.0467229,124.0028106",
     image: "/Bogo.jpg",
@@ -51,7 +51,7 @@ const branches = [
     address:
       "ASPAC BANK Bldg. Cor. N. Bacalso St. & P. Del Rosario Ext., Sambag 1 (Pob.) 6000 Cebu City (Capital) Cebu Philippines",
     contact: "(0917-102-5671)",
-    hours: "9:00 AM - 5:00 PM (Mon-Fri)",
+    hours: "9:00 AM - 3:00 PM (Mon-Fri)",
     mapUrl:
       "https://www.google.com/maps/place/10%C2%B017'34.3%22N+123%C2%B053'49.9%22E/@10.29285,123.897206,18z/data=!4m4!3m3!8m2!3d10.2928495!4d123.8972062?hl=en-US&entry=ttu&g_ep=EgoyMDI1MDQyNy4xIKXMDSoASAFQAw%3D%3D",
     image: "/.jpg",
@@ -61,7 +61,7 @@ const branches = [
     name: "Carcar Branch",
     address: "Poblacion II 6019 City of Carcar  Cebu Philippines",
     contact: "(0917-125-0313)",
-    hours: "9:00 AM - 5:00 PM (Mon-Fri)",
+    hours: "9:00 AM - 3:00 PM (Mon-Fri)",
     mapUrl:
       "https://www.google.com/maps?ll=10.104102,123.641838&z=18",
     image: "/Carcar.jpg",
@@ -72,7 +72,7 @@ const branches = [
     address:
       "ADM Commercial Bldg. Cansaga 6001 Consolacion Cebu Philippines",
     contact: "(0917-113-8143)",
-    hours: "9:00 AM - 5:00 PM (Mon-Fri)",
+    hours: "9:00 AM - 3:00 PM (Mon-Fri)",
     mapUrl:
       "https://www.google.com/maps/place/10%C2%B022'35.2%22N+123%C2%B057'36.7%22E/@10.376449,123.9602,18z",
     image: "/images/branches/consolacion.jpg",
@@ -82,7 +82,7 @@ const branches = [
     name: "Danao Branch",
     address: "Pio Del Pilar St. Poblacion 6004 Danao City Cebu Philippines",
     contact: "(0917-108-6575)",
-    hours: "9:00 AM - 5:00 PM (Mon-Fri)",
+    hours: "9:00 AM - 3:00 PM (Mon-Fri)",
     mapUrl:
       "https://www.google.com/maps/place/10%C2%B031'11.8%22N+124%C2%B001'36.0%22E/@10.519946,124.026669,18z",
     image: "/Danao.jpg",
@@ -92,7 +92,7 @@ const branches = [
     name: "Lapu-lapu Branch",
     address: "ASPAC BANK Bldg. Pusok 6015 Lapu-Lapu City Cebu Philippines",
     contact: "(0917-116-5655)",
-    hours: "9:00 AM - 5:00 PM (Mon-Fri)",
+    hours: "9:00 AM - 3:00 PM (Mon-Fri)",
     mapUrl:
       "https://www.google.com/maps/place/10%C2%B019'28.0%22N+123%C2%B058'27.3%22E/@10.324448,123.974253,18z/data=!4m4!3m3!8m2!3d10.3244481!4d123.9742525?hl=en-US&entry=ttu&g_ep=EgoyMDI1MDMyNS4xIKXMDSoASAFQAw%3D%3D",
     image: "/Lapu2.jpg",
@@ -103,7 +103,7 @@ const branches = [
     address:
       "ASPAC BANK Bldg. Cor. N. Bacalso St. & P. Del Rosario Ext., Sambag 1 (Pob.) 6000 Cebu City (Capital) Cebu Philippines",
     contact: "(0917-102-5671)",
-    hours: "9:00 AM - 5:00 PM (Mon-Fri)",
+    hours: "9:00 AM - 3:00 PM (Mon-Fri)",
     mapUrl:
       "https://www.google.com/maps/place/10%C2%B017'00.3%22N+123%C2%B053'05.0%22E/@10.283409,123.884722,18z",
     image: "/Nbacalso.jpg",
@@ -113,7 +113,7 @@ const branches = [
     name: "Talisay Branch",
     address: "#587 Natalio Bacalso Ave. Tabunoc 6045 City of Talisay Cebu Philippines",
     contact: "(0917-129-7008)",
-    hours: "9:00 AM - 5:00 PM (Mon-Fri)",
+    hours: "9:00 AM - 3:00 PM (Mon-Fri)",
     mapUrl:
       "https://www.google.com/maps/place/10%C2%B016'11.3%22N+123%C2%B050'41.5%22E/@10.269796,123.844864,18z",
     image: "/Talisay.jpg",
@@ -124,7 +124,7 @@ const branches = [
     address:
       "Purok Nangka, Sangi  Toledo City Cebu 6038 Philippines",
     contact: "(0917-129-7896)",
-    hours: "9:00 AM - 5:00 PM (Mon-Fri)",
+    hours: "9:00 AM - 3:00 PM (Mon-Fri)",
     mapUrl:
       "https://www.google.com/maps/place/10%C2%B023'10.8%22N+123%C2%B039'07.7%22E/@10.38633,123.65215,18z",
     image: "/Toledo.jpg",
