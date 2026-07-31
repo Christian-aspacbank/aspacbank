7f4da96b74dfd4a4c96e8d5f0d58db58b139dc1b
adrodriguez-aspacbank <adrodriguez@aspacbank.com>
2026-07-31 16:36:49 +0800

    Update SERVER_URL to new production endpoint

--- src/components/config/api.ts
+++ src/components/config/api.ts
@@ -1,1 +1,1 @@
-export const SERVER_URL = process.env.SERVER_URL || "https://rag-api-bank-production.up.railway.app";
+export const SERVER_URL = process.env.SERVER_URL || "https://rag-api-bank-production-d83e.up.railway.app";
